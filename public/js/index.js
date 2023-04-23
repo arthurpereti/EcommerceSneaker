14c5d09fdafec22f62450e4f36620e7553d5c787
Arthur Pereti <arthurcoelho2005@outlook.com>
2023-04-23 12:29:39 -0300

    melhorias na responsividade do index.html

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+// Função Lupa de Pesquisa
+
+let lupadepesquisa = document.getElementById('lupadepesquisa')
+
+lupadepesquisa.addEventListener('click', ()=>{
+
+})
+
+
+// Função Menu do Header
+
+
+let menuabertoheader = document.getElementById('menuabertoheader')
+
+menuabertoheader.addEventListener('click', ()=> {
+
+})
+
+
+// -------------------------------------- 
