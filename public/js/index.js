d1ad9cb90424800257d4505070acb95ade5748de
Arthur <arthurcoelho2005@outlook.com>
2023-04-27 09:45:46 -0300

    melhorias na responsividade v2.2, criação dos mode e loader

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 
 let lupadepesquisa = document.getElementById('lupadepesquisa')
 
-lupadepesquisa.addEventListener('click', ()=>{
+lupadepesquisa.addEventListener('click', () => {
 
 })
 
@@ -12,9 +12,22 @@ lupadepesquisa.addEventListener('click', ()=>{
 
 let menuabertoheader = document.getElementById('menuabertoheader')
 
-menuabertoheader.addEventListener('click', ()=> {
+menuabertoheader.addEventListener('click', () => {
 
 })
 
 
-// -------------------------------------- 
+// Botão que muda a cor do body
+
+let btnmudarcor = document.getElementById("btnmudarcor")
+btnmudarcor.addEventListener("click", () => {
+    let cor_fundo = document.getElementById("cor_fundo")
+    if (btnmudarcor.checked) {
+        cor_fundo.style.backgroundColor = 'black'
+    } else{
+        cor_fundo.style.backgroundColor = 'white'
+    }
+
+})
+
+
---
+++ public/js/loadinicial.js
@@ -0,0 +1,17 @@
+// window.addEventListener('load', () =>{
+//     document.body.style.backgroundColor= "white"
+// })
+
+// document.onreadystatechange = function() {
+//     if (document.readyState !== "complete") {
+//         document.querySelector(
+//           "body").style.visibility = "hidden";
+//         document.querySelector(
+//           "#spinner").style.visibility = "visible";
+//     } else {
+//         document.querySelector(
+//           "#spinner").style.display = "none";
+//         document.querySelector(
+//           "body").style.visibility = "visible";
+//     }
+// };
