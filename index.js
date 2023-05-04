eb9dc05cbc9f4fc122872f3002b55132bbcd16d5
Arthur Pereti <arthurcoelho2005@outlook.com>
2023-05-04 11:51:28 -0300

    local host funcionado e movi o index.js para a pasta public

--- index.js
+++ index.js
@@ -4,21 +4,8 @@ const app = express()
 const PORT = 3000
 const hostname = 'localhost'
 
-
-/* ----------config do express-------------- */
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-
 // Definindo que a pasta raiz do programa é o public
 app.use(express.static('public'))
-
-
-/* ----------arquivos estaticos-------------- */
-app.use(express.static('public'))
-/* ----------rotas do programa-------------- */
-app.get('/', (req,res)=>{
-    res.render('index.html')
-})
 
 
 // =======================================
