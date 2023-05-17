8b89ef843beab48a7104bdba4eccded606e179ac
Arthur Pereti <arthurcoelho2005@outlook.com>
2023-05-17 19:15:54 -0300

    alterações dos html para handlebars

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 
+const PORT = 3000
+const hostname = 'localhost'
+
 // ------ Config do express -------
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); // Definindo que a pasta raiz do programa é o public
 
@@ -14,13 +17,33 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 
-// Rotas do sistema
-app.get('/home', (req, res) => {
-    res.render('index.handlebars');
+// ------ Rotas do sistema ------
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.get('/teste', async (req,res)=>{
-    res.render('home.handlebars')
+app.get('/home', (req, res) => {
+    res.render('index');
+});
+
+app.get('/teste', async (req, res) => {
+    res.render('home');
+});
+
+app.get('/politicadecookie', async(req, res) =>{
+    res.render('politicadecookie')
 })
 
-app.listen(3000);
+app.get('/politicadeprivacidade', async (req, res) => {
+    res.render('politicadeprivacidade');
+});
+
+app.get('/politicadetroca', async(req, res) =>{
+    res.render('politicadetroca')
+})
+
+
+// =======================================
+app.listen(PORT, hostname, () => {
+    console.log(`Servidor Rodando em ${hostname}:${PORT}`)
+});
